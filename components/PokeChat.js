a487fbe84a6bf2850e2e87816d4e289446ced913
Zoilo Granda <zgranda@gmail.com>
2018-10-15 16:48:08 -0400

    2.4- Preguntas y Respuestas

--- components/PokeChat.js
+++ components/PokeChat.js
@@ -2,7 +2,6 @@ import React from 'react';
 import PokeMessage from './PokeMessage'
 
 export default class PokeChat extends React.Component {
-
   render(){
     return <ul className="PokeChat">
     {
