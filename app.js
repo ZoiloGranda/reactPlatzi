3a2bf487e803228d5431bda7f235f92ebe83f06c
Zoilo Granda <zgranda@gmail.com>
2018-10-19 09:32:01 -0400

    2.6- Manejo de datos en tiempo real con RethinkDB y ReactJS

--- app.js
+++ app.js
@@ -2,7 +2,8 @@ import React from 'react';
 import PokeApp from './components/PokeApp';
 
 
-
+//lo principal para renderizar la app de react, en cual elemento del DOM se va
+//a renderizar el componente padre de todos
 React.render(
   <PokeApp/>,
   document.getElementById('container')
